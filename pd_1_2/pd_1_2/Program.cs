ad93ecde69974bc989bd2041bc80029d19bded56
burkaans <raimonds.buholts@gmail.com>
2018-05-10 20:10:45 +0300

    pd_1_2
    2.commit

--- pd_1_2/pd_1_2/Program.cs
+++ pd_1_2/pd_1_2/Program.cs
@@ -10,8 +10,19 @@ namespace pd_1_2
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Ludzu ievadiet divas simbolu virknes, lai varu tas apvienot!");
+            string var1, var2;
+            var1 = Console.ReadLine();
+            var2 = Console.ReadLine();
+            string rezultats = Funkcija(var1, var2);
+            Console.WriteLine(rezultats);
             Console.ReadLine();
+        }
+        static string Funkcija(string vards1, string vards2)
+        {
+            string result ="jus ievadijat vardu " + vards1 +" un vardu " + vards2;
+            return result;
+
         }
     }
 }
